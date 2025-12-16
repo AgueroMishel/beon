911a32c91e39dd8f6602749ffaa6a0ff7cf05f95
AgueroMishel <m.aguero@outlook.com>
2025-12-16 09:36:06 -0600

    Foundation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # beon
+
+## Prerequisites
+* `npm` installed
+
+## Setup
+1. Clone and go the repo
+2. Execute `npm install cypress`
+3. Execute `npm install`
+
+## Execution
+**Window/Debug Mode:** `npx cypress open`
+**Headless Mode:** `npx cypress run`
---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: '',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/api/*.cy.js',
+    // Authentication Configuration
+    token: '',
+  },
+})
